e67b778b62e8cb9edc47e886fce63020775d0a08
gabrielmqc <gabrielmazzo2014@gmail.com>
2025-09-01 10:14:20 -0300

    feat: adding config and controllers

---
+++ src/main/java/com/test/ticket/config/TicketConfig.java
@@ -0,0 +1,7 @@
+package com.test.ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TicketConfig {
+}
---
+++ src/main/java/com/test/ticket/controllers/TicketController.java
@@ -0,0 +1,11 @@
+package com.test.ticket.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ticket")
+@CrossOrigin("*")
+public class TicketController {
+}
