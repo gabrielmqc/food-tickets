f9a41753c54e97d0c921eca2e360296e74ebe3c5
gabrielmqc <gabrielmazzo2014@gmail.com>
2025-09-05 08:39:24 -0300

    feat: adding CPF validation

--- api/src/main/java/com/test/ticket/application/dtos/EmployeeDTO.java
+++ api/src/main/java/com/test/ticket/application/dtos/EmployeeDTO.java
@@ -1,10 +1,11 @@
 package com.test.ticket.application.dtos;
 
 import com.test.ticket.domain.enums.Situation;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record EmployeeDTO(UUID id, String name, String cpf, Situation situation, LocalDateTime alterationDate, List<UUID> ticketsIds) {
+public record EmployeeDTO(UUID id, String name, @CPF String cpf, Situation situation, LocalDateTime alterationDate, List<UUID> ticketsIds) {
 }
